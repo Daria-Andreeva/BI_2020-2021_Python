bf9646aa62d6463a11db3e56bdb0ec288758d109
Daria Andreeva <daria1.andreeva@gmail.com>
2020-09-09 21:39:53 +0300

    calculator v2' git commit -m 'calculator v2'

--- calculator.py
+++ calculator.py
@@ -1,15 +1,51 @@
-number_1 = int(input('Please enter the first number: '))
-operation = input('Please enter the operation: ')
-number_2 = int(input('Please enter the second number: '))
-if operation == '+':
-    print('{} + {} = '.format(number_1, number_2))
-    print(number_1 + number_2)
-elif operation == '-':
-    print('{} - {} = '.format(number_1, number_2))
-    print(number_1 - number_2)
-elif operation == '*':
-    print('{} * {} = '.format(number_1, number_2))
-    print(number_1 * number_2)
-elif operation == '/':
-    print('{} / {} = '.format(number_1, number_2))
-    print(number_1 / number_2)
+#Give defenition to function calculate()
+def calculate():
+    #User input
+    number_1 = int(input('Please enter the first number: '))
+    operation = input('''
+    Please type in the math operation you would like to complete:
+    + for addition
+    - for subtraction
+    * for multiplication
+    / for division
+    ''')
+    number_2 = int(input('Please enter the second number: '))
+    #Calculation
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+# Give definition again() in calculate()
+def again():
+    #User decision if they want to do again
+    calc_again = input('''
+     Do you want to calculate again?
+    Please type Y for YES or N for NO.
+     ''')
+    if calc_again.upper() == 'Y':
+        calculate()
+    elif calc_again.upper() == 'N':
+        print('See you later.')
+    else:
+        again()
+
+# Give definition welcome()
+def welcome():
+    print('''Welcome to the Calculator! 
+    Have a nice calculation!''')
+
+# Do actual calculator
+welcome()
+calculate()
+again()
